f93845f6ab815e02003f5e961084cbd02536c50f
lihjay <llihjay@163.com>
2013-02-23 00:00:01 +0800

    chat js

--- public/Js/chat.js
+++ public/Js/chat.js
@@ -1,15 +1,32 @@
-/**
- * Created with JetBrains WebStorm.
- * User: lih
- * Date: 13-2-22
- * Time: 下午3:19
- * To change this template use File | Settings | File Templates.
- */
+var connection = new WebSocket('ws://localhost:3000');
+
 function regchatname()
-{
-    var username=$("#username").val();
+    {
+           var username=$("#username").val();
     $("#usernamecontent").css("display","none");
     $("#logok").html("当前登录的用户名为:"+username+"<br/><br/>");
     $("#msgdiv").css("display","block");
 }
 
+
+function sendmsg()
+{
+    var detail=$("#msgdetail").val();
+    if(msg)
+    {
+        connection.send(msg);
+    }
+
+}
+
+input.keydown(function(e) {
+    if (e.keyCode === 13) {
+        var msg = $(this).val();
+        if (!msg)    return;
+        connection.send(msg);
+        $(this).val('');
+        if (myName === false) {
+            myName = msg;
+        }
+    }
+});

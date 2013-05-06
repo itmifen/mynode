620ff8ce5041496179230f3b422b32b87baabbf7
lihjay <llihjay@163.com>
2013-05-06 23:31:06 +0800

    add joycookie.js ,no complete
    add joycookie.js ,no complete

---
+++ common/joycookie.js
@@ -0,0 +1,12 @@
+exports.setcookie=function(objName,objValue,objHours)
+{
+    var str = objName + "=" + escape(objValue);
+
+    if(objHours > 0){                               //为时不设定过期时间，浏览器关闭时cookie自动消失
+        var date = new Date();
+        var ms = objHours*3600*1000;
+        date.setTime(date.getTime() + ms);
+        str += "; expires=" + date.toGMTString();
+    }
+    document.cookie = str;
+}
